2101f14fc2be15ce09416bb676fbc0a1697be79e
John Kealy <johnckealy@gmail.com>
2020-05-12 16:22:40 +0100

    changed allowed hosts to new domain

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,6 @@
 from .defaults import *
 
 
-ALLOWED_HOSTS = ["134.209.183.91", "metwatch.club", "www.metwatch.club"]
+ALLOWED_HOSTS = ["134.209.183.91", "grib2netcdf.club", "www.grib2netcdf.club"]
 
 DEBUG = False
