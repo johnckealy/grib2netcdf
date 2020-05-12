00bd6608099d6554fbeef656a39169cc054ab6c3
John Kealy <johnckealy@gmail.com>
2020-05-12 18:02:26 +0100

    quick fix  of favicon bug

--- config/settings/defaults.py
+++ config/settings/defaults.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog.apps.BlogConfig",
-    "widget_tweaks",
-    'favicon',
+    "widget_tweaks"
 ]
 
 MIDDLEWARE = [
